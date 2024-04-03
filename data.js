b442e9985954ba6553a7588badd37a951bc3f5ab
Gistkin <80309998+Gistkin@users.noreply.github.com>
2024-04-03 17:46:12 +0500

    Commit

---
+++ data.js
@@ -0,0 +1,174 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-elt-huedepohl-aug2023-drone-pano1-360",
+      "name": "elt-huedepohl-aug2023-drone-pano1-360",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": -0.6813580485474056,
+        "pitch": 0.8295318756823598,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.41428067743801833,
+          "pitch": 1.1924432347306677,
+          "rotation": 0,
+          "target": "1-elt-huedepohl-aug2023-drone-pano2-360"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-elt-huedepohl-aug2023-drone-pano2-360",
+      "name": "elt-huedepohl-aug2023-drone-pano2-360",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": -0.03758332502298245,
+        "pitch": 1.0315286042896652,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.021876444729143,
+          "pitch": 1.4667938488164118,
+          "rotation": 0,
+          "target": "2-elt360-entrance-02"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-elt360-entrance-02",
+      "name": "elt360-entrance-02",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": 0.2617993877991527,
+        "pitch": -0.03525007820413073,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.3374028399396565,
+          "pitch": -0.03945011205456517,
+          "rotation": 0,
+          "target": "3-elt360-transfer-ring"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-elt360-transfer-ring",
+      "name": "elt360-transfer-ring",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": -1.6487145288582017,
+        "pitch": 0.03896061275194285,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7395396042901101,
+          "pitch": -0.06075168004884368,
+          "rotation": 0,
+          "target": "4-elt360-seismic-isolators"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-elt360-seismic-isolators",
+      "name": "elt360-seismic-isolators",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": -3.088382398974735,
+        "pitch": 0.23376367651168728,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "marzipano_test",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
