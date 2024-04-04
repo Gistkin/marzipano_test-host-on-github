0f042d853db5dccc5d570c7ee930395d65e701b8
Gistkin <80309998+Gistkin@users.noreply.github.com>
2024-04-04 12:50:34 +0500

    Enable fullscreen and control buttons

--- data.js
+++ data.js
@@ -168,7 +168,7 @@ var APP_DATA = {
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": false,
-    "fullscreenButton": false,
-    "viewControlButtons": false
+    "fullscreenButton": true,
+    "viewControlButtons": true
   }
 };
